639aad65128ab418b2d66a4a6ea7d79acd56f96e
Sean Pargeter <spargete@uoregon.edu>
2017-02-28 16:01:50 -0800

    Added implementation for Point

---
+++ src/Point.cpp
@@ -0,0 +1,28 @@
+#include "Point.hpp"
+
+Point::Point(Player* player, int x, int y) : player(player), x(x), y(y) {
+
+}
+
+Point::~Point(){}
+
+Player* Point::getPlayer() {
+	return this->player;
+}
+
+void Point::setPlayer(Player* setPlayer) {
+	player = setPlayer;
+}
+
+int Point::getX() {
+	return this->x;
+}
+int Point::getY() {
+	return this->y;
+}
+void Point::setX(int newX) {
+	x = newX;
+}
+void Point::setY(int newY) {
+	y = newY;
+}
